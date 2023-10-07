be528441533f3c54d84d55e51a14a219fa6bf76c
Anastasiya Ramanava1 <Anastasiya_Ramanava1@epam.com>
2023-10-07 15:06:43 +0300

    feat: add user without cart

--- src/user/repository.ts
+++ src/user/repository.ts
@@ -2,6 +2,7 @@ import { User } from '../types';
 
 const users: User[] = [
   { id: '0fe36d16-49bc-4aab-a227-f84df899a6cb' },
+  { id: 'eb5a26af-6e4c-4f31-a9b1-3450d42ac66c' },
 ];
 
 export const getUser = async (userId: User['id']): Promise<User | null> => {
